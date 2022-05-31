e5e7225e4cf25547f6a9c0d1296f19f4ad78217e
NerokArtworks <nerokprodscontact@gmail.com>
2022-05-31 11:26:38 +0200

    Custom Cursor - Cursor Load Animation - Home Title bugs fixed

--- js/anim.js
+++ js/anim.js
@@ -3,6 +3,7 @@ const body = document.body;
 const making = document.getElementById('load__making');
 const conLetters = document.getElementsByClassName('home__title')[0];
 const hLetters = document.querySelectorAll(".home__title__letter");
+const cursor = document.querySelector('.cls-1');
 
 // POINTER EVENTS STATE
 var activatePointers = false;
@@ -18,21 +19,34 @@ anime({
 // PAGE LOAD
 window.onload = () => {
     console.log('Loaded');
-    setTimeout(hideIntro, 5000);
-    setTimeout(showContent, 6000);
-    setTimeout(enablePointerEvents, 2000);
+    // Hide the intro animation
+    setTimeout(hideIntro, 2000);
+    // Show body and main title
+    setTimeout(showContent, 3000);
+    setTimeout(loadCursor, 2000);
+}
+
+function loadCursor() {
+    anime({
+        targets: cursor,
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'cubicBezier(0.770, 0.5, 0, 1.000);',
+        duration: 2500
+    });
 }
 
 function enablePointerEvents() {
     activatePointers = true;
+    // Create Event Listeners
+    hLetters.forEach(animateHoverLetter);
 }
 
 function showContent() {
     body.style.overflowY = "visible";
     conLetters.style.opacity = 1;
+    // Start title animation for each letter
     hLetters.forEach(animateShowLetter);
     console.log('Scroll alowed');
-    hLetters.forEach(animateHoverLetter);
 }
 
 function animateShowLetter(item, index) {
@@ -43,6 +57,8 @@ function animateShowLetter(item, index) {
         duration: 1500,
         easing: 'linear'
     })
+    // Enable pointer events after 'showing title' animation its finished so users can now hover on it
+    setTimeout(enablePointerEvents, 3000);
 }
 
 function hideIntro() {
@@ -51,13 +67,15 @@ function hideIntro() {
 
 // HOME LETTERS ANIMATION
 function animateHoverLetter(item, index) {
-    hLetters[index].addEventListener('mouseover', (e) => {
-        // if (activatePointers) {
-        //     console.log('Pointer Events Enabled')
-             hLetters[index].style.transition = 'none';
-        //     hLetters[index].style.pointerEvents = 'all';
-        // }
-        
+    // If true remove letters old transition and activate pointer events
+    if (activatePointers) {
+        console.log('Pointer Events Enabled');
+        hLetters[index].style.transition = 'none';
+        hLetters[index].style.pointerEvents = 'all';
+    }
+    
+    // Now the Event Listeners will correctly apply
+    hLetters[index].addEventListener('mouseover', (e) => { 
         anime({
             targets: hLetters[index],
             scaleY: 1.2,
@@ -75,3 +93,15 @@ function animateHoverLetter(item, index) {
         });
     })
 }
+
+// CURSOR
+const cursorRegular = document.querySelector('.cursor');
+
+const moveCursor = (e)=> {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+     
+    cursorRegular.style.transform = `translate3d(${mouseX - 22}px, ${mouseY - 22}px, 0)`;
+    // cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+}
+window.addEventListener('mousemove', moveCursor);
